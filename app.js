856393e53011449e69fc69743a8daa7bb5d043fa
Devatanu Deka <deva.deka@gmail.com>
2018-03-28 15:39:44 +1100

    basic win condition, logs true if a winning cond is found

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var board = []
 var cellList = document.querySelectorAll('.cell');
-//Load 
 
 var player = 0;
 var cellState = ['cell-player1','cell-player2','cell-empty'];
@@ -10,6 +9,7 @@ cellList.forEach(function(element){
         if(event.target.classList.contains(cellState[2])){
             event.target.classList.remove(cellState[2]);
             event.target.classList.add(cellState[player]);
+            console.log(checkWinCondition(cellState[player]));
             player = player^1; 
         }
         else{
@@ -18,3 +18,45 @@ cellList.forEach(function(element){
     });
 });
 
+var checkWinLine = function(index1,index2,index3, cellPlayer){
+    if( (cellList[index1].classList.contains(cellPlayer))&&
+        (cellList[index2].classList.contains(cellPlayer))&&
+        (cellList[index3].classList.contains(cellPlayer))  )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+var checkWinCondition = function(cellPlayer){
+    if(checkWinLine(0,1,2,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(3,4,5,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(6,7,8,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(0,3,6,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(1,4,7,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(2,5,8,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(0,4,8,cellPlayer)){
+        return true;
+    }
+    else if(checkWinLine(2,4,6,cellPlayer)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
