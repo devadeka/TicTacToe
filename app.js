63c1a2588218772cc482def8660d9e3e9abcba20
Devatanu Deka <deva.deka@gmail.com>
2018-03-28 11:25:50 +1100

    added logic for changing cell states, no repeat of cell selection

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 var board = []
-
+var cellList = document.querySelectorAll('.cell');
 //Load 
+
+var player = 0;
+var cellState = ['cell-player1','cell-player2','cell-empty'];
+
+cellList.forEach(function(element){
+    element.addEventListener('click',function(event){
+        if(event.target.classList.contains(cellState[2])){
+            event.target.classList.remove(cellState[2]);
+            event.target.classList.add(cellState[player]);
+            player = player^1; 
+        }
+        else{
+            console.log(event.target.classList);
+        }
+    });
+});
+
